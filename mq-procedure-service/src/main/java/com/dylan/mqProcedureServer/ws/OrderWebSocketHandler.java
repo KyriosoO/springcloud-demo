6db09487474c5fef8614d0f2d8354e09402cc44c
Kyrios <dalong.zhou@gmail.com>
2026-07-01 22:03:47 +0800

    Signed-off-by: Kyrios <dalong.zhou@gmail.com>

--- mq-procedure-service/src/main/java/com/dylan/mqProcedureServer/ws/OrderWebSocketHandler.java
+++
@@ -1,9 +0,0 @@
-package com.dylan.mqprocedureserver.ws;
-
-import org.springframework.stereotype.Component;
-
-import com.dylan.common.ws.support.CommonWebSocketHandler;
-
-@Component
-public class OrderWebSocketHandler extends CommonWebSocketHandler {
-}
